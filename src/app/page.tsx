82d8d3072bc828a3d5cf7efc9cbe62d73a93c5c5
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-15 20:05:56 -0300

    start to create login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Button from "@/components/ui/Button";
-import { db } from "@/lib/db";
 
 export default async function Home() {
-  return <Button>Hello</Button>;
+  return <Button variant={"ghost"}>Hello</Button>;
 }
