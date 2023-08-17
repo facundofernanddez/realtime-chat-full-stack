ed8d86bbaf4d0fdf21ee77d567d5c8f345fbcc2b
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-17 08:56:57 -0300

    add folder and create add friend page

---
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -0,0 +1,10 @@
+import AddFriendBotton from "@/components/AddFriendBotton";
+
+export default function AddFriendPage() {
+  return (
+    <main className="pt-8">
+      <h1 className="font-bold text-5xl mb-8">Add a friend</h1>
+      <AddFriendBotton />
+    </main>
+  );
+}
