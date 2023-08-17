614961e0ea81df612ab78ccf899f31b29218f802
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-17 08:57:10 -0300

    create component

---
+++ src/components/AddFriendBotton.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Button from "./ui/Button";
+
+export default function AddFriendBotton() {
+  return (
+    <form className="max-w-sm">
+      <label
+        htmlFor="email"
+        className="block text-sm font-medium leading-6 text-gray-9000"
+      >
+        Add friend by email
+      </label>
+      <div className="mt-2 flex gap-2">
+        <input
+          type="text"
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          placeholder="you@example.com"
+        />
+        <Button>Add</Button>
+      </div>
+    </form>
+  );
+}
