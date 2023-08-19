9b6a4552385b0c077bb6ddf27539719d746f1009
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-19 08:08:22 -0300

    create fetch function

--- src/helpers/redis.ts
+++ src/helpers/redis.ts
@@ -1,6 +1,26 @@
 const upstashRedisRestUrl = process.env.UPSTASH_REDIS_REST_URL;
 const authToken = process.env.UPSTASH_REDIS_REST_TOKEN;
 
-type Commands = "zrange" | "sismember" | "get" | "smembres";
+type Command = "zrange" | "sismember" | "get" | "smembres";
 
-export async function fetchRedis();
+export async function fetchRedis(
+  command: Command,
+  ...args: (string | number)[]
+) {
+  const commandUrl = `${upstashRedisRestUrl}/${command}/${args.join("/")}`;
+
+  const response = await fetch(commandUrl, {
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+    },
+    cache: "no-store",
+  });
+
+  if (!response.ok) {
+    throw new Error(`Error executing reids command: ${response.statusText}`);
+  }
+
+  const data = await response.json();
+
+  return data.result;
+}
