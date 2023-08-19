4f75e9848e93c6413990508e68b07e7a8102f587
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-19 10:31:59 -0300

    start to create layout

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,7 +1,21 @@
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import { ReactNode } from "react";
 
-export default async function Layout() {
+export default async function Layout({ children }: { children: ReactNode }) {
   const session = await getServerSession(authOptions);
-  return <div></div>;
+
+  if (!session) notFound();
+  return (
+    <div className="flex h-screen w-full">
+      <div className="flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6"></div>
+      <Link
+        href={"/dashboard"}
+        className="flex h-16 shrink-0 items-center"
+      ></Link>
+      {children}
+    </div>
+  );
 }
