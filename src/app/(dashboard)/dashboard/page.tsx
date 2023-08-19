e4d3328f0c16eb83df8fed407de32ed58f0462e2
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-19 08:10:05 -0300

    add use server and use client

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -4,8 +4,6 @@ import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 
 export default async function DashboardPage() {
-  "use server";
-
   const session = await getServerSession(authOptions);
 
   return <pre>{JSON.stringify(session)}</pre>;
