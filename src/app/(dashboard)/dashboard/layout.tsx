7cbf80991b0b80d25bbb1f7784aa5433b2800850
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-20 09:44:11 -0300

    create add friend option

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -16,7 +16,7 @@ const sidebarOptions: SidebarbarOption[] = [
   {
     id: 1,
     name: "Add friend",
-    href: "/dashboar/add",
+    href: "/dashboard/add",
     Icon: "UserPlus",
   },
 ];
@@ -53,7 +53,25 @@ export default async function Layout({ children }: { children: ReactNode }) {
               <ul
                 role="list"
                 className="-mx-2 mt-2 space-y-1"
-              ></ul>
+              >
+                {sidebarOptions.map((option) => {
+                  const Icon = Icons[option.Icon];
+                  return (
+                    <li key={option.id}>
+                      <Link
+                        href={option.href}
+                        className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                      >
+                        <span className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center roudend-lg border-text-[0.625rem] font-medium bg-white">
+                          <Icon className="h-4 w-4" />
+                        </span>
+
+                        <span className="truncate">{option.name}</span>
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
             </li>
           </ul>
         </nav>
