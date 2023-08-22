9b44e807493fbd1aac146db835bd3c92a9dbbe51
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-22 08:06:07 -0300

    fix typing error

--- src/helpers/redis.ts
+++ src/helpers/redis.ts
@@ -17,7 +17,7 @@ export async function fetchRedis(
   });
 
   if (!response.ok) {
-    throw new Error(`Error executing reids command: ${response.statusText}`);
+    throw new Error(`Error executing redis command: ${response.statusText}`);
   }
 
   const data = await response.json();
