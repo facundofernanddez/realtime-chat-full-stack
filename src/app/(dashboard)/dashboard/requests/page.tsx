717c73c0a0385f9f8b494864b9822ca5b2ae09ef
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-23 07:57:35 -0300

    refact error

--- src/app/(dashboard)/dashboard/requests/page.tsx
+++ src/app/(dashboard)/dashboard/requests/page.tsx
@@ -1,3 +1,4 @@
+import FriendRequests from "@/components/FriendRequests";
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
@@ -15,7 +16,7 @@ export default async function RequestsPage() {
 
   const incomingFriendRequests = await Promise.all(
     incomingSenderIds.map(async (senderId) => {
-      const sender = (await fetchRedis("get", `user/${senderId}`)) as User;
+      const sender = (await fetchRedis("get", `user:${senderId}`)) as User;
       return { senderId, senderEmail: sender.email };
     })
   );
@@ -23,7 +24,10 @@ export default async function RequestsPage() {
     <main className="pt-8">
       <h1 className="font-bold text-5xl mb-8">Add a friend</h1>
       <div className="flex flex-col gap-4">
-        <FriendRequests />
+        <FriendRequests
+          incomingFriendRequests={incomingFriendRequests}
+          sessionId={session.user.id}
+        />
       </div>
     </main>
   );
