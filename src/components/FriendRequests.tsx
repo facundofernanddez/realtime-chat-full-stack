34c9ddb59ccc174577df48e19dc1d8bcb695121c
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-24 08:49:21 -0300

    implement function to accept or deny friend request

--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import axios from "axios";
 import { Check, UserPlus, X } from "lucide-react";
-import { Cagliostro } from "next/font/google";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 interface FriendRequestsProps {
@@ -13,9 +14,28 @@ export default function FriendRequests({
   incomingFriendRequests,
   sessionId,
 }: FriendRequestsProps) {
+  const router = useRouter();
   const [friendRequests, setFriendRequests] = useState<IncomingFriendRequest[]>(
     incomingFriendRequests
   );
+
+  const acceptFriend = async (senderId: string) => {
+    await axios.post("/api/friends/accept", { id: senderId });
+
+    setFriendRequests((prev) =>
+      prev.filter((request) => request.senderId !== senderId)
+    );
+    router.refresh();
+  };
+
+  const denyFriend = async (senderId: string) => {
+    await axios.post("/api/friends/deny", { id: senderId });
+
+    setFriendRequests((prev) =>
+      prev.filter((request) => request.senderId !== senderId)
+    );
+    router.refresh();
+  };
 
   return (
     <>
@@ -31,6 +51,7 @@ export default function FriendRequests({
             <p className="font-medium text-lg">{request.senderEmail}</p>
             <button
               aria-label="accept friend"
+              onClick={() => acceptFriend(request.senderId)}
               className="w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md"
             >
               <Check className="font-semibold text-white w-3/4 h-3/4" />
@@ -38,6 +59,7 @@ export default function FriendRequests({
 
             <button
               aria-label="deny friend"
+              onClick={() => denyFriend(request.senderId)}
               className="w-8 h-8 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md"
             >
               <X className="font-semibold text-white w-3/4 h-3/4" />
