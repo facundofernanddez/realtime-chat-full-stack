9a01d228eefaf969a33d190e5e46fbd5fbb5355c
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-26 10:45:35 -0300

    fetch chat by user id

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -1,5 +1,7 @@
+import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { messageArrayValidator } from "@/lib/validations/message";
 import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
 
@@ -8,6 +10,27 @@ interface PageProps {
     chatId: string;
   };
 }
+
+const getChatMessages = async (chatId: string) => {
+  try {
+    const result: string[] = await fetchRedis(
+      "zrange",
+      `chat:${chatId}:messages`,
+      0,
+      -1
+    );
+
+    const dbMessages = result.map((message) => JSON.parse(message) as Message);
+
+    const resversedDbMessages = dbMessages.reverse();
+
+    const messages = messageArrayValidator.parse(resversedDbMessages);
+
+    return messages;
+  } catch (error) {
+    notFound();
+  }
+};
 
 export default async function OneChatPage({ params }: PageProps) {
   const { chatId } = params;
@@ -23,7 +46,7 @@ export default async function OneChatPage({ params }: PageProps) {
 
   const chatPartnerId = user.id === userId1 ? userId2 : userId1;
   const chatPartner = (await db.get(`user:${chatPartnerId}`)) as User;
-  const initialMessages = await getChatMessage(chatId);
+  const initialMessages = await getChatMessages(chatId);
 
   return <div>{params.chatId}</div>;
 }
