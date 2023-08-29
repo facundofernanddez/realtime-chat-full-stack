8ed47c8f1c4ac5c9c2e546b7bf08824bfe73bcaa
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-29 07:43:34 -0300

    trigger pusher

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -1,6 +1,8 @@
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { pusherServer } from "@/lib/pusher";
+import { toPusherKey } from "@/lib/utils";
 import { getServerSession } from "next-auth";
 import { z } from "zod";
 
@@ -25,6 +27,15 @@ export async function POST(req: Request) {
     if (isAlreadyFriend) {
       return new Response("Already friends", { status: 400 });
     }
+
+    pusherServer.trigger(
+      toPusherKey(`user:${idToAdd}:incoming_friend_request`),
+      "incoming_friend_request",
+      {
+        senderId: session.user.id,
+        senderEmail: session.user.email,
+      }
+    );
 
     const hasFriendRequest = await fetchRedis(
       "sismember",
