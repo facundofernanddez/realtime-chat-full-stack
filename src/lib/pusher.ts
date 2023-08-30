90c9f03ec94b175e9e891aef5fdc45ce8fc719b7
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-30 07:38:58 -0300

    add access to client next app key

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -3,14 +3,14 @@ import PusherClient from "pusher-js";
 
 export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID! as string,
-  key: process.env.PUSHER_APP_KEY! as string,
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY! as string,
   secret: process.env.PUSHER_APP_SECRET! as string,
   cluster: "us2",
   useTLS: true,
 });
 
 export const pusherClient = new PusherClient(
-  process.env.PUSHER_APP_KEY! as string,
+  process.env.NEXT_PUBLIC_PUSHER_APP_KEY! as string,
   {
     cluster: "us2",
   }
