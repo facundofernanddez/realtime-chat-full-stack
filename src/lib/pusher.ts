be1680bb076247e150e95826949d6bc04e7b0e96
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-31 09:04:59 -0300

    refact

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -2,15 +2,15 @@ import PusherServer from "pusher";
 import PusherClient from "pusher-js";
 
 export const pusherServer = new PusherServer({
-  appId: process.env.PUSHER_APP_ID! as string,
-  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY! as string,
-  secret: process.env.PUSHER_APP_SECRET! as string,
+  appId: process.env.PUSHER_APP_ID!,
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+  secret: process.env.PUSHER_APP_SECRET!,
   cluster: "us2",
   useTLS: true,
 });
 
 export const pusherClient = new PusherClient(
-  process.env.NEXT_PUBLIC_PUSHER_APP_KEY! as string,
+  process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   {
     cluster: "us2",
   }
