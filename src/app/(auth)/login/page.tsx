4212f85c55a4eb4e79e8a0afe4109c2ece673276
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-09-04 08:39:04 -0300

    add logo

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Icons } from "@/components/Icons";
 import Button from "@/components/ui/Button";
 import { signIn } from "next-auth/react";
 import { useState } from "react";
@@ -26,7 +27,7 @@ export default function LoginPage() {
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 ">
         <div className="w-full flex flex-col items-center max-w-md space-y-8">
           <div className="flex flex-col items-center gap-8">
-            logo
+            <Icons.Logo className="text-indigo-600" />
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
               Sign in to your account
             </h2>
