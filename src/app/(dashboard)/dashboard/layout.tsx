20ee22406284227c7777eb4ecc649af521cb1606
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-09-04 10:07:46 -0300

    change import

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -2,7 +2,7 @@ import FriendRequestsSideBarOption from "@/components/FriendRequestsSideBarOptio
 import MobileLayout from "@/components/MobileChatLayout";
 import SidebarChatList from "@/components/SidebarChatList";
 import SignOutButton from "@/components/SignOutButton";
-import { Icon, Icons } from "@/components/Icons";
+import { Icons } from "@/components/Icons";
 import { getFriendsByUserId } from "@/helpers/getFriend";
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
