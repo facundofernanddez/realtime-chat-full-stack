8191867887d53b685afe1f5e9e6276f646dd5346
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-09-04 08:20:17 -0300

    add pusher trigger in friends api

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -1,6 +1,8 @@
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { pusherServer } from "@/lib/pusher";
+import { toPusherKey } from "@/lib/utils";
 import { addFriendValidator } from "@/lib/validations/add-friend";
 import { getServerSession } from "next-auth";
 import { z } from "zod";
@@ -52,6 +54,16 @@ export const POST = async (req: Request) => {
     }
 
     //valid request, send friend request
+
+    await pusherServer.trigger(
+      toPusherKey(`user:${idToAdd}:incoming_friend_request`),
+      "incoming_friend_request",
+      {
+        senderId: session.user.id,
+        senderEmail: session.user.email,
+      }
+    );
+
     db.sadd(`user:${idToAdd}:incoming_friend_request`, session.user.id);
 
     return new Response("OK");
